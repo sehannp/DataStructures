9a03d1c36f905df4ab9c9fa186b7ff1991fa8e0d
sehannp <sehanp@gmail.com>
2020-06-20 01:23:55 +1000

    added remove method

--- MyTree.js
+++ MyTree.js
@@ -1,3 +1,9 @@
+/* 
+  Author: Sehan
+  Description: This is a model implementation of Binary tree for JS
+*/
+
+// A object for each node of the tree
 class Node {
     constructor(value){
       this.left = null;
@@ -7,14 +13,17 @@ class Node {
 }
   
 class BinarySearchTree {
+
   constructor(){
     this.root = null;
   }
+
   insert(value){
     if (this.root === null){
       this.root = new Node(value);
     }
     else {
+        //reference to the current node. This is not necessarily the root. At start it is the root.
         let currentNode = this.root;
         while(true){
           if (value < currentNode.value){
@@ -23,6 +32,7 @@ class BinarySearchTree {
               currentNode.left = new Node(value);
               return this;
             }
+            //there is a value already. So you are moving the reference to the next level down.
             currentNode = currentNode.left;
           }
           else {
@@ -37,9 +47,105 @@ class BinarySearchTree {
     }
   }
   lookup(value){
-    //Code here
+      if(!this.root){
+        return false;
+      }
+      let currentNode = this.root;
+      while(currentNode){
+        if(value === currentNode.value){
+          return currentNode;
+        }
+        else if(value < currentNode.value){
+          currentNode = currentNode.left;
+        }
+        else if((value > currentNode.value)){
+          currentNode = currentNode.right;
+        }
+      }
+      return null;
   }
-  // remove
+  
+  remove(value){
+    if(!this.root){
+      return false;
+    }
+    let currentNode = this.root;
+    let parentNode = null;
+    while(currentNode){
+      if(value < currentNode.value){
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      }
+      else if (value  > currentNode.value){
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      }
+      else if (currentNode.value === value){
+
+        //if no right child
+        if(currentNode.right === null){
+          // for cases when root is the one to be removed
+          if(parentNode === null){
+            this.root = currentNode.left;
+          }
+          //decides where to attach the child after node is removed.
+          else{
+            if(currentNode.value < parentNode.value){
+              parentNode.left = currentNode.left;
+            }
+            else if (currentNode.value > parentNode.value){
+              //what is there is something on parent's right. Wouldnt it get replaced ?
+              parentNode.right = currentNode.left;
+            }
+          }
+        }
+        //right child doesnt have a left child
+        else if(currentNode.right.left === null){
+          if(parentNode === null){
+            this.root = currentNode.left;
+          }
+          else{
+            currentNode.right.left = currentNode.left;
+            if(currentNode.value < parentNode.value){
+              parentNode.left = currentNode.right;
+            }
+            else if (currentNode.value > parentNode.value){
+              //what is there is something on parent's right. Wouldnt it get replaced ?
+              parentNode.right = currentNode.right;
+            }  
+          }
+        }
+        //right child has a left child
+        else {
+          // find the right child's left most child
+          let leftmost = currentNode.right.left;
+          let leftmostParent = currentNode.right;
+          while(leftmost.left !== null){
+            leftmostParent = leftmost;
+            leftmost = leftmost.left;
+          }
+
+          leftmostParent.left = leftmost.right;
+          leftmost.left = currentNode.left;
+          leftmost.right = currentNode.right;
+
+          if(parentNode === null){
+            this.root = leftmost;
+          }
+          else {
+            if(currentNode.value < parentNode.value){
+              parentNode.left = leftmost;
+            }
+            else if (currentNode.value > parentNode.value){
+              //what is there is something on parent's right. Wouldnt it get replaced ?
+              parentNode.right = leftmost;
+            } 
+          }
+        }
+        return true;
+      }
+    }
+  }
 }
   
   const tree = new BinarySearchTree();
@@ -52,6 +158,7 @@ class BinarySearchTree {
   tree.insert(1)
   console.log(JSON.stringify(traverse(tree.root)));
   
+  console.log(tree.lookup(20));
   //     9
   //  4     20
   //1  6  15  170
