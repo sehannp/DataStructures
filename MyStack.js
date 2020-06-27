9c4b53da2d7fd39e0f8bd935bbef320953c5fadc
sehannp <sehanp@gmail.com>
2020-06-27 22:03:00 +1000

    implementation of stack on JS

---
+++ MyStack.js
@@ -0,0 +1,104 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+
+// Implementation of stack on using LinkedList
+class Stack {
+
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value){
+      if(this.length === 0){
+        this.top = new Node(value);
+        this.bottom = this.top;
+      }
+      else{
+        let newNode = new Node(value);
+        let topNode = this.top;
+        this.top = newNode;
+        this.top.next = topNode;
+      }
+      this.length++;
+      return this;
+  }
+  pop(){
+    if (this.length === 0) {
+        return null;
+    }
+    let rmvNode = this.top;
+    this.top = this.top.next;
+    this.length--;
+
+    //if this is not done, then on removal this.bottom will still point to the old node.
+    if (this.length === 1 ){
+        this.bottom = null;
+    }
+
+    return rmvNode;
+  }
+
+  isEmpty(){
+    return this.length === 0;      
+  }
+}
+
+// const myStack = new Stack();
+// myStack.push("Discord");
+// myStack.push("Udemy");
+// myStack.push("google");
+// console.log(myStack.pop());
+// console.log(myStack.pop());
+// console.log(myStack.pop());
+// console.log(myStack.isEmpty());
+//Discord
+//Udemy
+//google
+
+
+
+// Implementation of stack on using Arrays
+class StackArray {
+
+    constructor(){
+      this.stackArray = [];
+    }
+
+    peek() {
+      return this.stackArray[this.stackArray.length-1];
+    }
+    push(value){
+        this.stackArray.push(value);
+        return this;
+    }
+    pop(){
+      if (this.stackArray.length === 0) {
+          return null;
+      }
+      this.stackArray.pop();
+      return this;
+    }
+  
+    isEmpty(){
+      return (this.stackArray.length === 0);      
+    }
+  }
+
+
+  const myStack = new StackArray();
+  myStack.push("Discord");
+  myStack.push("Udemy");
+  myStack.push("google");
+  console.log(myStack.pop());
+  console.log(myStack.pop());
+  console.log(myStack.pop());
+  console.log(myStack.isEmpty());
